29be97409518edba67f0e9b3b58dd705c2d17163
maximka <maximka>
2025-02-18 17:14:12 +0300

    add project structure and default files

---
+++ src/game/game.py
@@ -0,0 +1,21 @@
+import pygame
+
+FPS=144
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((800, 600),pygame.NOFRAME)
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.screen.fill((58, 58, 58))
+            pygame.display.flip()
+            self.clock.tick(FPS)
+
+        pygame.quit()
